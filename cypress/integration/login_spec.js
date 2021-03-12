d07e5b3d94e2b50f451ab4cd856ebebcbbe15fb3
Matti Tuomikoski <mtuomikoski@gmail.com>
2021-03-12 17:26:01 +0200

    At wits end

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -3,23 +3,43 @@ const baseUrl = `http://localhost:${appPort}`
 
 
 it('Login request', function () {
+  // Global intercept
+  // cy.intercept('GET', '*').as('getEverything')
+
+  // API login intercept
   cy.intercept('POST', '/api/login').as('loginUser')
+
+  // Reset DB
+  cy.request('POST', `${baseUrl}/api/testing/reset`)
+  const user = {
+    username: 'tiinatest',
+    name: 'Tiina Testeri',
+    password: 'pass',
+  }
+  // Create user
+  cy.request('POST', `${baseUrl}/api/users`, user)
   cy.visit(baseUrl)
 
-  // cy.get('[data-cy=login-username]').type('foobar')
-  // cy.get('[data-cy=login-password]').type('foobaz')
-  // cy.get('[data-cy=login-button]').click()
+  // Wait GET loads
+  //cy.wait('@getEverything')
+  cy.wait(10000)
 
-  cy.request({
-    method: 'POST',
-    url: '/api/login',
-    body: { username: 'foobar2', password: 'foobaz2' },
-    failOnStatusCode: false,
-  }).then((response) => {
-    cy.log(response)
-  })
-  // cy.wait('@loginUser').then((interception) => {
-  //   cy.log(interception.response.body)
+  cy.get('[data-cy=login-username]').type('foobar')
+  cy.wait(1000)
+  cy.get('[data-cy=login-password]').type('foobaz')
+  cy.wait(1000)
+  cy.get('[data-cy=login-button]', { timeout: 10000 }).click()
+
+  // cy.request({
+  //   method: 'POST',
+  //   url: '/api/login',
+  //   body: { username: 'foobar2', password: 'foobaz2' },
+  //   failOnStatusCode: false,
+  // }).then((response) => {
+  //   cy.log(response)
   // })
+  cy.wait('@loginUser').then((interception) => {
+    cy.log(interception.response.body)
+  })
 })
 
