f529277c115c38e7dd1453d1a6874cecd3fa423e
Matti Tuomikoski <mtuomikoski@gmail.com>
2021-03-12 17:12:55 +0200

    Make manual api/login request in Cypress

--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -6,12 +6,20 @@ it('Login request', function () {
   cy.intercept('POST', '/api/login').as('loginUser')
   cy.visit(baseUrl)
 
-  cy.get('[data-cy=login-username]').type('foobar')
-  cy.get('[data-cy=login-password]').type('foobaz')
-  cy.get('[data-cy=login-button]').click()
+  // cy.get('[data-cy=login-username]').type('foobar')
+  // cy.get('[data-cy=login-password]').type('foobaz')
+  // cy.get('[data-cy=login-button]').click()
 
-  cy.wait('@loginUser').then((interception) => {
-    cy.log(interception.response.body)
+  cy.request({
+    method: 'POST',
+    url: '/api/login',
+    body: { username: 'foobar2', password: 'foobaz2' },
+    failOnStatusCode: false,
+  }).then((response) => {
+    cy.log(response)
   })
+  // cy.wait('@loginUser').then((interception) => {
+  //   cy.log(interception.response.body)
+  // })
 })
 
